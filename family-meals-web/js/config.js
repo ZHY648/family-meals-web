d5858862353ebc81d272d366602b7419b8b6e84c
ZHY648 <1163392682@qq.com>
2026-05-29 21:56:19 +0800

    Update config.js

--- family-meals-web/js/config.js
+++ family-meals-web/js/config.js
@@ -4,6 +4,6 @@
  * 也可直接改下面两行（部署到 Vercel 时用环境变量更好）
  */
 window.APP_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://bbhcxibujlytxcicbqtt.supabase.co/rest/v1/',
+  supabaseAnonKey: 'sb_publishable_Tk62srabzyap-3SPvCSdWA_ZYEMkBK-'
 }
